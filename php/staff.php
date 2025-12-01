22a3fa26ea9fc56b13bad6ace300533b4e8f53ac
Gwen288 <wendy.dwumfuor@ashesi.edu.gh>
2025-12-01 01:53:10 +0000

    updated staff php

--- php/staff.php
+++ php/staff.php
@@ -1,8 +1,6 @@
 <?php
+// david's plain text password used for authentication
 $staff1=password_hash("david.mensah29",PASSWORD_DEFAULT);
-
-$staff2=password_hash("sarah.agyei27",PASSWORD_DEFAULT);
-
-echo $staff1.'   '.$staff2;
+echo $staff1;
 
 ?>
